f3d60ebbd858b28223caa53bdecd9faaafb99e8f
FullOfOrange <foorgdev@gmail.com>
2020-01-02 15:58:54 +0900

    feat: intergrate all node small toy projects

--- pipeline/main.js
+++ pipeline/main.js
@@ -1,28 +1,30 @@
 let data = [
-    {
-        id: 1,
-        name: 'kim'
-    },{
-        id: 2,
-        name: 'lee'
-    },{
-        id: 3,
-        name: 'park'
-    },{
-        id: 4,
-        name: 'choi'
-    }
+  {
+    id: 1,
+    name: "kim"
+  },
+  {
+    id: 2,
+    name: "lee"
+  },
+  {
+    id: 3,
+    name: "park"
+  },
+  {
+    id: 4,
+    name: "choi"
+  }
 ];
-const A = (data) => new Promise(resolve =>
-    setTimeout(() =>
-        resolve(data.map((cur) => (cur.id)))
-    ,1)
-);
-const B = (data) => new Promise(resolve =>
-    setTimeout(() => 
-        resolve(data.reduce((initVal,cur) => initVal + cur,0))
-    ,1)
-);
+const A = data =>
+  new Promise(resolve => setTimeout(() => resolve(data.map(cur => cur.id)), 1));
+const B = data =>
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve(data.reduce((initVal, cur) => initVal + cur, 0)),
+      1
+    )
+  );
 /*
 A(data).then(id => {
     B(id).then(name => {
@@ -36,12 +38,10 @@ A(data).then(id => {
     console.log(await B(d));
 })();
 */
-function* gen(){
-    let id = yield A(data);
-    let result = yield B(id);
-    console.log(result);
+function* gen() {
+  let id = yield A(data);
+  let result = yield B(id);
+  console.log(result);
 }
 const g = gen();
-g.next().value.then(id =>
-    g.next(id).value.then(result =>
-        g.next(result)));
+g.next().value.then(id => g.next(id).value.then(result => g.next(result)));
